6653794ab4105421b11b04cb72d401a393086987
massi-01 <massi.gatta@gmail.com>
2023-03-04 17:56:07 +0100

    added the id field

--- app/src/main/java/com/example/todo/ToDoObject.java
+++ app/src/main/java/com/example/todo/ToDoObject.java
@@ -1,12 +1,22 @@
 package com.example.todo;
 
 public class ToDoObject {
+    int id;
     String title;
     String date;
 
-    public ToDoObject(String title, String date) {
+    public ToDoObject(int id,String title, String date) {
+        this.id = id;
         this.title = title;
         this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
