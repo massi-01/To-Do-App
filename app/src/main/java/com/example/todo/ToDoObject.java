1ba978a1421f9ac17bf7ac120e71322b8ce39e59
massi-01 <massi.gatta@gmail.com>
2023-03-04 11:34:30 +0100

    removed the id field

--- app/src/main/java/com/example/todo/ToDoObject.java
+++ app/src/main/java/com/example/todo/ToDoObject.java
@@ -1,29 +1,11 @@
 package com.example.todo;
 
 public class ToDoObject {
-    int id;
     String title;
     String date;
 
-    public ToDoObject(int id, String title, String date) {
-        this.id = id;
+    public ToDoObject(String title, String date) {
         this.title = title;
-        this.date = date;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDate(String date) {
         this.date = date;
     }
 
