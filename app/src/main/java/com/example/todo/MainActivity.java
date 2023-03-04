035a7fbfbd132d83a309a564e2917a58bcbdaa7e
massi-01 <massi.gatta@gmail.com>
2023-03-04 11:33:20 +0100

    Added a custom adapter for the list of todos and implemented the add button

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     public ListView listView;
     public List<ToDoObject> toDoList;
+    public MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listView);
         toDoList = new ArrayList<>();
-        MyAdapter myAdapter = new MyAdapter(this, R.layout.list_element, toDoList);
+
+        myAdapter = new MyAdapter(this, R.layout.list_element, toDoList);
+
+        listView.setAdapter(myAdapter);
     }
 
     public void pressedAddButton(View view) {
-        //TODO
-        new AlertDialog.Builder(getApplicationContext()).setTitle("Add new");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.dialog_layout, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setView(dialogView);
+
+
+        builder.setPositiveButton("OK", (dialog, id) -> {
+            EditText titleEditText = dialogView.findViewById(R.id.task_edit_text);
+            DatePicker datePicker = dialogView.findViewById(R.id.date_picker);
+            String date = datePicker.getDayOfMonth() + "/"
+                    + (datePicker.getMonth() + 1) + "/"
+                    + datePicker.getYear();
+
+            toDoList.add(new ToDoObject(titleEditText.getText().toString(), date));
+            myAdapter.notifyDataSetChanged();
+        });
+
+        builder.setNegativeButton("Cancel", (dialog, id) -> {
+
+        });
+
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
     }
 }
